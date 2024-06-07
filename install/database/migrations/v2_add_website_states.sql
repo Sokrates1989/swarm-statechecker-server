b4f98defd2a8b90e16d491bf8e1656bdc28cd44f
Patrick Michiels <it@patrickmichiels.de>
2024-06-07 21:59:04 +0200

    implemented and finalized database migration adding website states

--- install/database/migrations/v2_add_website_states.sql
+++ install/database/migrations/v2_add_website_states.sql
@@ -1,16 +1,24 @@
--- Check if the migration has already been applied
-IF NOT EXISTS (SELECT 1 FROM schema_migrations WHERE migration = 'V2__add_website_states') THEN
-        
-    -- Migration SQL
-    USE state_checker;
-    CREATE TABLE IF NOT EXISTS `checked_websites` (
-        `ID` BIGINT NOT NULL AUTO_INCREMENT,
-        `name` TEXT NOT NULL DEFAULT '',
-        `state` TEXT NOT NULL DEFAULT '',
-        `messageHasBeenSent` TINYINT NOT NULL DEFAULT '0',
-        PRIMARY KEY (`ID`)
-    ) ENGINE=InnoDB;
+DELIMITER //
 
-    -- Record the migration in schema_migrations table
-    INSERT INTO schema_migrations (migration) VALUES ('V2__add_website_states');
-END IF;
+CREATE PROCEDURE apply_add_website_states_migration()
+BEGIN
+    -- Check if the migration has already been applied.
+    IF NOT EXISTS (SELECT 1 FROM schema_migrations WHERE migration = 'V2__add_website_states') THEN
+            
+        -- Migration SQL.
+        CREATE TABLE IF NOT EXISTS `checked_websites` (
+            `ID` BIGINT NOT NULL AUTO_INCREMENT,
+            `name` TEXT NOT NULL,
+            `state` TEXT NOT NULL,
+            `messageHasBeenSent` TINYINT NOT NULL DEFAULT '0',
+            PRIMARY KEY (`ID`)
+        ) ENGINE=InnoDB;
+
+        -- Record the migration in schema_migrations table.
+        INSERT INTO schema_migrations (migration) VALUES ('V2__add_website_states');
+    END IF;
+END//
+DELIMITER ;
+
+CALL apply_add_website_states_migration();
+DROP PROCEDURE apply_add_website_states_migration;
